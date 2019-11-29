8b31d7cf7c3337cebcaff22b6403a00272018d29
Thai Doan <doannamthai123@gmail.com>
2019-11-29 14:39:26 -0700

    Improve the accuracy up to 99%

--- MNISTModel.py
+++ MNISTModel.py
@@ -9,23 +9,30 @@ import tensorflow as tf
 import numpy as np
 
 print("--Get data--")
-with np.load("notMNIST.npz", allow_pickle=True) as f:
-    x_train, y_train = f['x_train'], f['y_train']
-    x_test, y_test = f['x_test'], f['y_test']
-
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 print("--Process data--")
 print(len(y_train))
+# Reshaping
+x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+# Normalize
 x_train, x_test = x_train / 255.0, x_test / 255.0
- 
+
 print("--Make model--")
 model = tf.keras.models.Sequential([
-  tf.keras.layers.Flatten(input_shape=(28, 28)),
-  tf.keras.layers.Dense(10, activation='sigmoid')
+    tf.keras.layers.Conv2D(28, kernel_size=(3, 3), input_shape=(28, 28, 1)),
+    tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(10, activation='softmax'),
 ])
-model.compile(optimizer='sgd', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 print("--Fit model--")
-model.fit(x_train, y_train, epochs=1, verbose=2)
+model.fit(x_train, y_train, epochs=10, verbose=2)
 
 print("--Evaluate model--")
 model_loss, model_acc = model.evaluate(x_test,  y_test, verbose=2)
